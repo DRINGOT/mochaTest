0b9be20088358a0fdac0d63aacf6876c8886f826
DRINGOT <donovan.ringot@hotmail.fr>
2019-12-04 09:55:32 +0100

    :white_check_mark:  create test for sumResult1

--- test/app.test.js
+++ test/app.test.js
@@ -3,5 +3,11 @@ const app = require ("../app");
 
 const sumResult1 = app.sum(5,5)
 
-
+describe ("app", function(){
+    describe("sum", function(){
+        it("sum should return 10", function(){
+            assert.equal(sumResult1, 10);
+        });
+    });
+});
 
