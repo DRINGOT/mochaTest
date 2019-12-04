b8ed329823d1efb552b5f76f24649033167c48a0
DRINGOT <donovan.ringot@hotmail.fr>
2019-12-04 10:13:14 +0100

    :white_check_mark:  create another sum test and check if result is a number

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,20 @@ const assert = require ("chai").assert;
 const app = require ("../app");
 
 const sumResult1 = app.sum(5,5)
+const sumResult2 = app.sum(-1000,1001)
+const sumResult3 = app.sum(0,0)
+
 
 describe ("app", function(){
     describe("sum", function(){
         it("sum should return 10", function(){
             assert.equal(sumResult1, 10);
+        });
+        it ("sum should return 1001",function (){
+            assert.equal(sumResult2, 1);
+        });
+        it ("sum should be a Number types", function(){
+            assert.typeOf(sumResult3, "number")
         });
     });
 });
